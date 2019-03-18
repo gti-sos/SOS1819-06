b3b59e4e6d7a0f698bef221264e2a3d160ab0282
sos1819jhp <jesherpal1@alum.us.es>
2019-03-18 11:52:56 +0000

    updated

--- index.js
+++ index.js
@@ -299,6 +299,177 @@ app.put("/api/v1/transfer-stats", (req,res)=>{
     res.sendStatus(404);
 });
 
+//Recursos Jesús Herrera
+
+var uefaCountryRankings = [];
+
+// GET /api/v1/uefa-country-rankings/loadInitialData
+
+app.get("/api/v1/uefa-country-rankings/loadInitialData", (req,res)=>{
+    
+    uefaCountryRankings =  [{
+    country: "Spain",
+    season: "17-18",
+    rankingPosition : 1,
+    points : 106998,
+    teams : 7
+},{
+    country: "Spain",
+    season: "16-17",
+    rankingPosition : 1,
+    points : 104998,
+    teams : 7
+}, {
+    country: "England",
+    season: "17-18",
+    rankingPosition : 2,
+    points : 79605,
+    teams : 7
+}, {
+    country: "Germany",
+    season: "16-17",
+    rankingPosition : 2,
+    points : 79498,
+    teams : 7
+}];
+
+    res.sendStatus(200);
+});
+
+
+// GET /api/v1/uefa-country-rankings
+
+app.get("/api/v1/uefa-country-rankings", (req,res)=>{
+    res.send(uefaCountryRankings);
+});
+
+
+// POST /api/v1/uefa-country-rankings
+
+app.post("/api/v1/uefa-country-rankings", (req,res)=>{
+    
+    var newUefaCountryRankings = req.body;
+    
+    uefaCountryRankings.push(newUefaCountryRankings);
+    
+    res.sendStatus(201);
+});
+
+
+// DELETE /api/v1/uefa-country-rankings
+
+app.delete("/api/v1/uefa-country-rankings", (req,res)=>{
+    
+    uefaCountryRankings =  [];
+
+    res.sendStatus(200);
+});
+
+
+// GET /api/v1/uefa-country-rankings/Spain
+
+app.get("/api/v1/uefa-country-rankings/:country", (req,res)=>{
+
+    var country = req.params.country;
+
+    var filteredUefaCountryRankings = uefaCountryRankings.filter((c) =>{
+       return c.country == country; 
+    });
+    
+    if (filteredUefaCountryRankings.length >= 1){
+        res.send(filteredUefaCountryRankings);
+    }else{
+        res.sendStatus(404);
+    }
+
+});
+
+// GET /api/v1/uefa-country-rankings/Spain/17-18
+
+app.get("/api/v1/uefa-country-rankings/:country/:season", (req,res)=>{
+
+    var country = req.params.country;
+    var season = req.params.season;
+
+    var filteredUefaCountryRankings = uefaCountryRankings.filter((c) =>{
+       return c.country == country && c.season==season; 
+    });
+    
+    if (filteredUefaCountryRankings.length >= 1){
+        res.send(filteredUefaCountryRankings);
+    }else{
+        res.sendStatus(404);
+    }
+
+});
+
+
+// PUT /api/v1/uefa-country-rankings/Spain
+
+app.put("/api/v1/uefa-country-rankings/:country", (req,res)=>{
+
+    var country = req.params.country;
+    var updatedUefaCountryRankings = req.body;
+    var found = false;
+
+    var updatedUefaCountryRankings2 = uefaCountryRankings.map((c) =>{
+    
+        if(c.country == country){
+            found = true;
+            return updatedUefaCountryRankings;
+        }else{
+            return c;            
+        }
+ 
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        uefaCountryRankings = updatedUefaCountryRankings2;
+        res.sendStatus(200);
+    }
+
+});
+
+
+// DELETE /api/v1/uefa-country-rankings/Spain
+
+app.delete("/api/v1/uefa-country-rankings/:country", (req,res)=>{
+
+    var country = req.params.country;
+    var found = false;
+
+    var updatedCountry = uefaCountryRankings.filter((c) =>{
+        
+            if(c.country == country)  
+                found = true;
+        
+            return c.country != country;
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        uefaCountryRankings = updatedCountry;
+        res.sendStatus(200);
+    }
+
+});
+
+// POST /api/v1/uefa-country-rankings/Spain
+
+app.post("/api/v1/uefa-country-rankings/:country", (req,res)=>{
+
+    res.sendStatus(409);
+});
+
+// PUT /api/v1/uefa-country-rankings
+
+app.put("/api/v1/uefa-country-rankings", (req,res)=>{
+
+    res.sendStatus(409);
+});
 
 app.listen(port,()=>{
     console.log("Magic is happening in port "+port);
