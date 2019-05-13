69be632b486986010b32eb0d7c8bf136997d5743
sos1819jeg <javier.ezcurra.garrido@gmail.com>
2019-05-13 09:56:22 +0000

    prueba

--- tests/TC07-loadDatauefaclub.js
+++ tests/TC07-loadDatauefaclub.js
@@ -1,7 +1,7 @@
 describe('Data is loaded', function () {
 	it('should show a bunch of data', function (){
 		browser.get('http://localhost:8080');
-		var uefaclubs= element.all(by.repeater('uefaclub in uefaclubs'));
-		expect(uefaclubs.count()).toBeGreaterThan(0);
+		var uefaclubs= element.all(by.repeater('uefaclub in uefaclubrankigs'));
+		expect(uefaclub.count()).toBeGreaterThan(0);
 	});
 });
--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,5 @@
 exports.config = {
-	seleniumAddress: 'http://localhost:9515/wd/hub',
+	seleniumAddress: 'http://localhost:8080/wd/hub',
 	chromeOnly: true,
 	specs: [
 		'TC01-loadDatauefacountry.js',
