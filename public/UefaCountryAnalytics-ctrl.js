7d3fefe4746f7a6dc91fafb7666c2768786822a8
sos1819jhp <jesherpal1@alum.us.es>
2019-05-21 11:39:44 +0000

    update

---
+++ public/UefaCountryAnalytics-ctrl.js
@@ -0,0 +1,12 @@
+/*global angular*/
+/*global Highcharts*/
+/*global google*/
+/*global uv*/
+angular
+    .module("ManagerApp")
+    .controller("UefaCountryAnalytics-ctrl", ["$scope", "$http", function($scope, $http) {
+
+        console.log("main Controller initialized");
+
+        
+    }]);
