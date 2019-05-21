b335e820daa1abf55cc71a4ee18ca4ae2c986c0e
sos1819abl <alfobralla@hotmail.com>
2019-05-21 20:15:43 +0000

    a

---
+++ public/transferStatsAnalytics-ctrl.js
@@ -0,0 +1,12 @@
+/*global angular*/
+/*global Highcharts*/
+/*global google*/
+/*global uv*/
+angular
+    .module("ManagerApp")
+    .controller("transferStatsAnalytics-ctrl", ["$scope", "$http", function($scope, $http) {
+
+        console.log("Analytics Controller initialized");
+
+        
+    }]);
